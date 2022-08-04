6669b3b298302b3a2066b091372e6bbdf2902983
Sewell Stephens <sewellstephens104@gmail.com>
2022-08-04 18:10:17 -0500

    Create example.js

---
+++ example.js
@@ -0,0 +1,7 @@
+// replace FEATURE_HERE and PLAN_NAME with desired values
+
+window.location.href('https://example.com/reserved-page?feat=FEATURE_HERE&plan=PLAN_NAME')
+
+// Use dynamic values instead like this
+
+window.location.href('https://example.com/reserved-page?feat=' + FEATURE_HERE '&plan=' + PLAN_NAME)
